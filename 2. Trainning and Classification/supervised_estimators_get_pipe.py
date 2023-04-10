afa95d8e763133c854b6b0748112ce87cc98e79e
Noon Abdulqadir (MFA Noon) <noon.abdulqadir@gmail.com>
2023-04-10 13:02:01 +0200

    Update supervised_estimators_get_pipe.py

--- 2. Trainning and Classification/supervised_estimators_get_pipe.py
+++ 2. Trainning and Classification/supervised_estimators_get_pipe.py
@@ -431,7 +431,7 @@ et = make_pipe_list(et_, et_params)
 gbc_ = GradientBoostingClassifier()
 gbc_params = {
     'random_state': [random_state],
-    'loss': ['log_loss', 'deviance', 'exponential'],
+    'loss': ['log_loss', 'log_loss', 'exponential'],
     # 'max_features': ['auto'],
 }
 gbc = make_pipe_list(gbc_, gbc_params)
