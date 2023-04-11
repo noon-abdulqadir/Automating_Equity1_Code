fac8679e741004a2ffb1065508bb8639603d8174
Noon Abdulqadir (MFA Noon) <noon.abdulqadir@gmail.com>
2023-04-11 12:01:48 +0200

    .

--- 2. Trainning and Classification/1. supervised_estimators_train_nonlinear.py
+++ 2. Trainning and Classification/1. supervised_estimators_train_nonlinear.py
@@ -39,7 +39,11 @@ method = 'Supervised'
 classifiers_list = classifiers_list_nonlinear
 classifiers_pipe = classifiers_pipe_nonlinear
 results_save_path = f'{models_save_path}{method} Results/'
+if not os.path.exists(results_save_path):
+    os.makedirs(results_save_path)
 done_xy_save_path = f'{results_save_path}Search+Xy/'
+if not os.path.exists(done_xy_save_path):
+    os.makedirs(done_xy_save_path)
 t = time.time()
 n_jobs = -1
 n_splits = 10
